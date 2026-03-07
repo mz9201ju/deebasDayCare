6a57a1e740247596400ec2715a7d064f16cab656
omer.zahid@mnsu.edu <omer.zahid@mnsu.edu>
2026-03-07 00:06:22 -0800

    base updated to /

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/deebasDayCare/", // ⬅ important for GH Pages under a subpath
+  base: "/", // ⬅ important for GH Pages under a subpath
 })
